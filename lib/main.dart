95d12699c987f8810b4c2305ff2b511342df3960
mahm00dmahm00d <mahmoud.mahmoud.2000.01.01@gmail.com>
2022-12-26 15:30:27 +0300

    Update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_consulting_platform/screens/categories/categories_screen.dart';
-import 'package:flutter_consulting_platform/screens/login/login_screen.dart';
-import 'package:flutter_consulting_platform/screens/register/register_screen.dart';
-import 'package:get/get.dart';
+import '/core/dependencies/bindings.dart';
+import '/screens/categories/categories_screen.dart';
+import '/screens/login/login_screen.dart';
+import '/screens/specialists/specialists_screen.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'core/constants.dart';
 import 'core/material_app.dart';
-import 'core/fonts.dart';
-import 'screens/homepage/homepage.dart';
-import 'screens/shared/application_buttons.dart';
-import 'screens/shared/input_fields.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  GetStorage.init();
+  await GetStorage.init();
 
   runApp(
-    const MyApp(
-      home: LoginScreen(),
-
-    ),
+    const MyApp(),
   );
 }
