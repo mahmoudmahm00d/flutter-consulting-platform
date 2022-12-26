da3fca0ce2211169b9575f46eed7471e6f2c3969
mahm00dmahm00d <mahmoud.mahmoud.2000.01.01@gmail.com>
2022-12-26 15:08:22 +0300

    Update constants file

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,15 @@
 class Constants {
-  static const String baseURL = '';
+  static const String baseURL = 'http://172.27.128.1:8000/api/';
   static const String applicationTitle = 'Full Consulting';
+}
+
+class ApiEndPoints {
+  // Auth
+  static const String login = 'auth/login';
+  static const String register = 'auth/register';
+  // App
+  static const String specialists = "specialists";
+  static const String categories = "categories";
+  static const String specialistProfile = "specialists/";
+  static const String checkAppointment = "appointments/check";
 }
