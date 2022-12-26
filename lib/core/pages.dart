57a216bef9c47d5191d9de5e657bf9501d81dbea
mahm00dmahm00d <mahmoud.mahmoud.2000.01.01@gmail.com>
2022-12-26 15:29:20 +0300

    Add get pages

---
+++ lib/core/pages.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+
+import '/core/dependencies/bindings.dart';
+import '/core/constants.dart';
+import '/screens/categories/categories_screen.dart';
+import '/screens/homepage/homepage.dart';
+import '/screens/login/login_screen.dart';
+import '/screens/register/register_screen.dart';
+
+List<GetPage> pages = [
+  GetPage(
+    name: '/home',
+    page: () => const MyHomePage(title: Constants.applicationTitle),
+  ),
+  GetPage(
+    name: '/login',
+    page: () => const LoginScreen(),
+    binding: LoginBindings(),
+  ),
+  GetPage(
+    name: '/register',
+    page: () => const RegisterScreen(),
+    binding: RegisterBindings(),
+  ),
+  GetPage(
+    name: '/categories',
+    page: () => const CategoriesScreen(),
+    binding: CategoriesBindings(),
+  ),
+];
