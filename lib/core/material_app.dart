253c05dd913921a5640d424759d9756cec58e9ff
mahm00dmahm00d <mahmoud.mahmoud.2000.01.01@gmail.com>
2022-12-26 15:29:53 +0300

    Update material app

--- lib/core/material_app.dart
+++ lib/core/material_app.dart
@@ -2,36 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
+import '../screens/unknown_route/unknown_route_screen.dart';
+import 'pages.dart';
+import 'theme.dart';
 import 'constants.dart';
 import 'localizations.dart' as locale;
 
 class MyApp extends StatelessWidget {
-  final Widget home;
-  const MyApp({super.key, required this.home});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.cupertino,
       title: Constants.applicationTitle,
       translations: locale.Localizations(),
-      locale: GetStorage().read("locale") == 'en'
-          ? const Locale('en', 'US')
-          : const Locale('ar', 'SY'),
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
+      locale: GetStorage().read("locale") == 'ar'
+          ? const Locale('ar', 'SY')
+          : const Locale('en', 'US'),
+      theme: appTheme,
+      getPages: pages,
+      unknownRoute: GetPage(
+        name: '/unknown',
+        page: () => const UnknownRouteScreen(),
       ),
-      // Remove focus from TextFormField
-      home: GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.focusedChild!.unfocus();
-          }
-        },
-        child: home,
-      ),
+      initialRoute: '/login',
     );
   }
 }
